6a174254600d80a6361bd3b9c11351b9f25dc108
Aymeric <aymericschweitzer@gmail.com>
2019-10-16 09:54:53 +0200

    Commentaire + modif constructeur d'arc

--- src/Arc.java
+++ src/Arc.java
@@ -12,12 +12,32 @@ public class Arc {
 	// 2: Empty Edges arc
 	private int type;
 	
+	
+	/**
+	 * 
+	 * @param Weight corresponds to the amount of tokens that will be moved after firing
+	 * @param Place corresponds to the Place linked to the arc
+	 * @param Transition corresponds to the Transition linked to the arc
+	 * @param Type provides information about the type of arc
+	 */
 	public Arc(int Weight, Place Place, Transition Transition, int Type) {
-		if (((Type!=-1) && (Type!=0) && (Type!=1) && (Type!=2) ) || (weight<0)) {
+		if (((Type!=-1) && (Type!=1)) || (weight<0)) {
 			throw new IllegalArgumentException("Input Data are wrong");
 		}
 		else {
 		weight = Weight;
+		place = Place;
+		transition = Transition;
+		type = Type;
+		}
+	}
+	
+	public Arc(Place Place, Transition Transition, int Type) {
+		if (((Type!=0) && (Type!=2))) {
+			throw new IllegalArgumentException("Input Data are wrong");
+		}
+		else {
+		weight = 0;
 		place = Place;
 		transition = Transition;
 		type = Type;
