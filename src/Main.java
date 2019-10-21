e73e1f1bd9609922412bc678ff6af81a05bd5cab
Nicolas Scholten <nicolasscholten@live.fr>
2019-10-21 11:01:41 +0200

    Ajout README et Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main class in which the user creates it's PetriNet
+ * @author SCHOLTEN_SCHWEITZER_ROULLIER
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
